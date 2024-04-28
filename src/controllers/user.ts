5e05d51e247d03bf2619a8f4c65d359b6a095dd3
Abhiyan Upreti <abhiyan@noveltytechnology.com>
2024-04-28 17:17:04 +0545

    error handler

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,10 +1,12 @@
 import { NextFunction, Request, Response } from 'express'
 import { UserModel } from '../models/User.js'
 import { TNewUserRequestBody } from '../types/user.js'
+import { ErrorHandler } from '../utils/utility-class.js'
 
 export const newUser = async (req: Request<{}, {}, TNewUserRequestBody>, res: Response, next: NextFunction) => {
   try {
     const { name, email, photo, gender, _id, dob } = req.body
+    if (!name) return next(new ErrorHandler('mero message', 201))
     const user = await UserModel.create({ name, email, photo, gender, _id, dob: new Date(dob) })
     return res.status(200).json({
       success: true,
