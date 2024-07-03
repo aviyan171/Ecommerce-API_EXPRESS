c50d3397908ea23eb4bde569915b05b5175c6ebc
abhiyanNovelty1 <abhiyan@noveltytechnology.com>
2024-07-03 13:52:13 +0545

    pie chart

--- src/routes/stats.ts
+++ src/routes/stats.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getDashBoardStats } from '../controllers/stats.js'
+import { getDashBoardStats, getPieChartStats } from '../controllers/stats.js'
 import { isAdmin } from '../middlewares/auth.js'
 
 const statsRouter = express.Router()
 
 statsRouter.get('/', isAdmin('view dashboard stats'), getDashBoardStats)
+statsRouter.get('/pie', isAdmin('view pie'), getPieChartStats)
 
 export { statsRouter }
